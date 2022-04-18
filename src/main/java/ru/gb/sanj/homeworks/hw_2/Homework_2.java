72e65b618c4e19410502f880fea173d796b76058
sanj_akhmedov <1999.sanjar.06@gmail.com>
2022-04-18 06:34:42 +0500

    Task 1 is done

--- src/main/java/ru/gb/sanj/homeworks/hw_2/Homework_2.java
+++ src/main/java/ru/gb/sanj/homeworks/hw_2/Homework_2.java
@@ -1,7 +1,12 @@
 package ru.gb.sanj.homeworks.hw_2;
 
 public class Homework_2 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println(checkNum(10));
+    }
+// Task 1
+    public static boolean checkNum (int num) {
+        return num >= 10 && num <= 20;
     }
 }
